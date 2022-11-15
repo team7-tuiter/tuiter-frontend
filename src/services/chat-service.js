b4adca12fbc582b275898bd8bf449396ef8379ea
javier-rosas <jr2dj@virginia.edu>
2022-11-15 10:33:10 -0500

    fixed api routes in chat-service

--- src/services/chat-service.js
+++ src/services/chat-service.js
@@ -4,15 +4,14 @@ const BASE_URL = "http://localhost:4000"
 const TUITS_API = `${BASE_URL}/api/tuits`
 const USERS_API = `${BASE_URL}/api/users`
 
-export const apiGetSingleChat = () => 
-  axios.get("http://localhost:4000/api/tuits")
-  .then(response => response.data)
+export const apiGetSingleChat = (uid1, uid2) => 
+  axios.get(`${USERS_API}/${uid1}/users/${uid2}/chat`)
+  .then(response => response.data[0])
 
-export const apiGetAllChatsById = (tid) =>
-  axios.get(`${TUITS_API}/${tid}`)
+export const apiGetAllChatsById = (uid) =>
+  axios.get(`${USERS_API}/${uid}/chats`)
     .then(response => response.data)
 
-export const apiDeleteSingleChat = (uid) =>
-  axios.get(`${USERS_API}/${uid}/tuits`)
+export const apiDeleteSingleChat = (uid1, uid2) =>
+  axios.delete(`${USERS_API}/${uid1}/users/${uid2}/chat`)
     .then(response => response.data)
-    
