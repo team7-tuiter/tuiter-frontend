9547731095ac3e7b0165af2fc1127783ef532f39
Saket Kumar <kumar.sak@northeastern.edu>
2022-11-16 14:46:22 -0500

    Configured the registration to use firebase auth service

--- src/components/profile/login.js
+++ src/components/profile/login.js
@@ -1,21 +1,15 @@
-import {Link, useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import * as service from "../../services/users-service";
+import { registerUsingUsername } from '../../services/auth-service';
 import React from "react";
-import {UserList} from "./user-list";
+import { UserList } from "./user-list";
 
 export const Login = () => {
   const [existingUsers, setExistingUsers] = useState([]);
-    // {username: 'user1', email: 'user1', password: 'user1', _id: '123'}
-  // ]);
   const [newUser, setNewUser] = useState({});
   const [loginUser, setLoginUser] = useState({});
   // const navigate = useNavigate()
-
-  const uuu = [
-    {username: 'ellen_ripley', email: 'ellen_ripley', password: 'ellen_ripley', _id: '123'},
-    {username: 'sarah', email: 'ellen_ripley', password: 'ellen_ripley', _id: '234'}
-  ]
 
   const deleteUser = (uid) =>
     service.deleteUser(uid)
@@ -25,9 +19,16 @@ export const Login = () => {
       .then(users => {
         setExistingUsers(users)
       })
-  const register = () =>
-    service.createUser(newUser)
-      .then(findAllUsers);
+  const register = async () => {
+    try {
+      let userCredential = await registerUsingUsername(newUser._username, newUser._password);
+    } catch (e) {
+      console.error(e);
+      alert(e.message);
+    }
+  }
+  service.createUser(newUser)
+    .then(findAllUsers);
   const login = () =>
     service.findUserByCredentials(loginUser)
       .then((user) => {
@@ -38,34 +39,30 @@ export const Login = () => {
     <div>
       <h1>Register</h1>
       <input className="mb-2 form-control"
-             onChange={(e) =>
-               setNewUser({...newUser, _username: e.target.value})}
-             placeholder="username"/>
+        onChange={(e) =>
+          setNewUser({ ...newUser, _username: e.target.value })}
+        placeholder="username" />
       <input className="mb-2 form-control"
-             onChange={(e) =>
-               setNewUser({...newUser, _password: e.target.value})}
-             placeholder="password" type="password"/>
-      <input className="mb-2 form-control"
-             onChange={(e) =>
-               setNewUser({...newUser, _email: e.target.value})}
-             placeholder="email" type="email"/>
+        onChange={(e) =>
+          setNewUser({ ...newUser, _password: e.target.value })}
+        placeholder="password" type="password" />
       <button onClick={register} className="btn btn-primary mb-5">Register
       </button>
 
       <h1>Login</h1>
       <input className="mb-2 form-control"
-             onChange={(e) =>
-               setLoginUser({...loginUser, _username: e.target.value})}
-             placeholder="username"/>
+        onChange={(e) =>
+          setLoginUser({ ...loginUser, _username: e.target.value })}
+        placeholder="username" />
       <input className="mb-2 form-control"
-             onChange={(e) =>
-               setLoginUser({...loginUser, _password: e.target.value})}
-             placeholder="password" type="password"/>
+        onChange={(e) =>
+          setLoginUser({ ...loginUser, _password: e.target.value })}
+        placeholder="password" type="password" />
       <button onClick={login} className="btn btn-primary mb-5">Login</button>
 
       <h1>Login As</h1>
 
-      <UserList users={existingUsers} deleteUser={deleteUser}/>
+      <UserList users={existingUsers} deleteUser={deleteUser} />
 
     </div>
   );
