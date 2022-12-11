00b7d947226db2481b611ac18884252a8ad4202c
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-12-11 06:39:08 -0500

    added changes to display images on the frontend

--- src/components/messages/index.js
+++ src/components/messages/index.js
@@ -5,64 +5,70 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { sendMessage, receiveMessage } from "../../redux/messageSlice";
-import { getLastMessagesFromAllChats } from "../../redux/chatSlice"
+import { getLastMessagesFromAllChats } from "../../redux/chatSlice";
 import { storage } from "../../firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import SocketFactory from "../../socket";
-import uuid from 'react-uuid';
-
+import uuid from "react-uuid";
 
 const Messages = () => {
-  const messageList = useSelector((state) => state.messages.messages)
-  const dispatch = useDispatch()
-  const [message, setMessage] = useState("")
-  const room = useSelector((state) => state.room.room)
-  const [file, setFile] = useState(null)
-  const user = useSelector((state) => state.user.user)
-  const chats = useSelector((state) => state.chats.chats)
+  const messageList = useSelector((state) => state.messages.messages);
+  const dispatch = useDispatch();
+  const [message, setMessage] = useState("");
+  const room = useSelector((state) => state.room.room);
+  const [file, setFile] = useState(null);
+  const user = useSelector((state) => state.user.user);
+  const chats = useSelector((state) => state.chats.chats);
 
   const send = async () => {
-    if (!room) return 
-    const [userId1, userId2] = room.split(" -- ")
-    const to = [userId1, userId2].filter(uid => uid !== user._id)[0]
+    console.log(file.type);
+    if (!room) return;
+    const [userId1, userId2] = room.split(" -- ");
+    const to = [userId1, userId2].filter((uid) => uid !== user._id)[0];
     const payload = {
       room,
       userId1,
       userId2,
       messages: {
-        id: uuid().slice(0,8),
+        id: uuid().slice(0, 8),
         from: user._id,
         to: to,
-        type : "STRING",
+        type: "STRING",
         message,
-        sentOn: Date.now()
-      }
-    }
+        sentOn: Date.now(),
+      },
+    };
     if (file) {
-      const fileref = ref(storage, `static/${Date.now()}`)
-      uploadBytes(fileref, file).then((res) => {
-        getDownloadURL(res.ref).then((url) => {
-          payload.messages.message = url
-          payload.messages.type = "IMAGE"
-          dispatch(sendMessage(payload));
-          setFile(null);
-        })
-      })
+      const fileref = ref(storage, `static/${Date.now()}`);
+      const upload = await uploadBytes(fileref, file);
+      payload.messages.message = await getDownloadURL(upload.ref);
+      payload.messages.type = file.type.split("/")[0].toUpperCase();
+      console.log(payload);
+      dispatch(sendMessage(payload));
+      setFile(null);
+      // uploadBytes(fileref, file).then((res) => {
+      //   getDownloadURL(res.ref).then((url) => {
+      //     payload.messages.message = url
+      //     payload.messages.type = "IMAGE"
+      //     dispatch(sendMessage(payload));
+      //     setFile(null);
+      //   })
+      // })
     }
     if (message) {
-      dispatch(sendMessage(payload))
-      setMessage("")
+      dispatch(sendMessage(payload));
+      setMessage("");
     }
     if (messageList.length === 1) {
-      dispatch(getLastMessagesFromAllChats(user._id))
+      dispatch(getLastMessagesFromAllChats(user._id));
     }
-  }
+  };
 
   useEffect(() => {
     SocketFactory.getConnection().on("receive_message", (message) => {
       dispatch(receiveMessage(message));
-    })
-  }, [dispatch])
+    });
+  }, [dispatch]);
 
   return (
     <div>
@@ -94,23 +100,66 @@ const Messages = () => {
       <button onClick={send}> Send Message </button>
 
       {messageList.map((message, i) => {
-        if (i === 0) return
-        const isMe = message.from === user._id
+        if (i === 0) return;
+        const isMe = message.from === user._id;
         if (isMe) {
           return (
             <div key={i.toString()} className="text-primary float-right">
-            <p> {message.message} {message.sentOn} </p>
-          </div>
-          )
+              <p>
+                {" "}
+                {message.type === "STRING" && (
+                  <>
+                    {message.message} {message.sentOn}{" "}
+                  </>
+                )}
+                {message.type === "IMAGE" && (
+                  <img
+                    src={message.message}
+                    alt={message.id}
+                    style={{ maxWidth: "320px", maxHeight: "240px" }}
+                  ></img>
+                )}
+                {message.type === "VIDEO" && (
+                  <video
+                    src={message.message}
+                    width="320"
+                    height="240"
+                    controls
+                  >
+                    Your browser does not support the video tag.
+                  </video>
+                )}
+              </p>
+            </div>
+          );
         }
         return (
           <div key={i.toString()} className="text-secondary float-left">
-            <p> {message.message} {message.sentOn} </p>
+            <p>
+              {" "}
+              {message.type === "STRING" && (
+                <>
+                  {message.message} {message.sentOn}{" "}
+                </>
+              )}
+              {message.type === "IMAGE" && (
+                <img
+                  src={message.message}
+                  alt={message.id}
+                  style={{ maxWidth: "320px", maxHeight: "240px" }}
+                ></img>
+              )}
+              {message.type === "VIDEO" && (
+                <video src={message.message} width="320" height="240" controls>
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </p>
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
